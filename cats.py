25b58fcc0b91dd27f016510cb7491da4e6f0d8ea
Maksim Strekoza <maksim_mail@list.ru>
2021-04-01 19:59:39 +1100

    cat5 added

--- cats.py
+++ cats.py
@@ -4,9 +4,10 @@ cat1 = Cat("Vera","g",5)
 cat2 = Cat("Sam","b",2)
 cat3 = Cat("Sally","g",3)
 cat4 = Cat("Ben","b",6)
+cat5 = Cat("Jessy","g",53)
 
 
-cats=[cat1, cat2, cat3, cat4]
+cats=[cat1, cat2, cat3, cat4, cat5]
 
 for cat in cats:
     cat.show_cat()
